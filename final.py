38e41f297366561628a6988276a691d0b7c13bb2
Shaheer764 <shaheerfareed085@gmail.com>
2025-05-21 07:26:57 -0700

    Add files via upload

---
+++ final.py
@@ -0,0 +1,10 @@
+if comp_choice == "paper" :
+    print("paper covers rock: computer win")
+else:
+    print("rock smashes scissor:you win")
+
+elif user_choice == "paper"
+     if comp_choice == "scissor":
+         print("scissor cut paper: compuer win")
+    else:
+        print("paper covers rock: you win")
